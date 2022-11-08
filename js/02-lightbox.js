e74576db808e0c6bace2b6e1951332b328ae0851
Ielithabeth <lisa.pltrtskaya@gmail.com>
2022-11-08 23:32:00 +0200

    2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,7 +7,7 @@ const gallery = document.querySelector(".gallery");
 const imagesLinks = document.querySelectorAll("a.gallery__link");
 
 const addImages = galleryItems.map(image =>
-    `<a class="gallery__item" href="${image.original}"><img class = "gallery__image" loading="lazy" src = "${image.preview}" data-source = "${image.original}" alt = "${image.description}" width = "200"></a>`).join(" ");
+    `<a class="gallery__item" href="${image.original}"><img class = "gallery__image" loading="lazy" src = "${image.preview}" alt = "${image.description}" width = "200"></a>`).join(" ");
 
 gallery.insertAdjacentHTML('afterbegin', addImages);
 
